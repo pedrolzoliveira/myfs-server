4a07bfb7505ff6614414fe3440137e25682611b1
Pedro Oliveira <pluiz@mychirotouch.com>
2022-11-09 18:16:09 -0300

    error handler

--- src/infra/http/express/middlawares/error-handler.ts
+++ src/infra/http/express/middlawares/error-handler.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response } from 'express'
+import { HttpError } from '../../http-error'
 import { HttpStatusCode } from '../../http-status-code'
 import { transformResponse } from '../../transformers/response-transformer'
 
 export class ErrorHandler {
   handle(error: Error, req: Request, res: Response, next: NextFunction) {
+    if (error instanceof HttpError) {
+      return res.status(error.statusCode).send(
+        transformResponse({
+          errors: [error],
+          message: error.message,
+          ok: false
+        })
+      )
+    }
     return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(
       transformResponse({
         errors: [error],
