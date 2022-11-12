a862a39182b9e6852cdb6162aec3512223cbf3e7
Pedro Oliveira <pluiz@mychirotouch.com>
2022-11-12 20:41:08 -0300

    better naming

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globalSetup: './scripts/reset-db.js',
-  globalTeardown: './scripts/delete-temp-files.js'
+  globalSetup: './scripts/jest-setup.js',
+  globalTeardown: './scripts/jest-teardown.js'
 }
