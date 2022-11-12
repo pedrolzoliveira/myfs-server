48386fd51fab4f075e2bef273a9e7b38825219e5
Pedro Oliveira <pluiz@mychirotouch.com>
2022-11-12 17:58:41 -0300

    changing evs

--- scripts/reset-db.js
+++ scripts/reset-db.js
@@ -1,8 +1,11 @@
 const { PrismaClient } = require('@prisma/client')
 
 async function main () {
-  process.env.DATABASE_URL = process.env.TEST_DATABASE_URL
-  console.log('\ndeleting db...\n')
+  console.log('\nchanging envs...')
+  process.env.DATABASE_URL = process.env.DATABASE_URL_TEST
+  process.env.MULTER_DEST = process.env.MULTER_DEST_TEST
+
+  console.log('deleting db...')
   const prismaClient = new PrismaClient()
   await prismaClient.file.deleteMany()
   await prismaClient.folder.deleteMany()
