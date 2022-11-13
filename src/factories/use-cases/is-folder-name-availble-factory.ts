a50d5c1aadae53c7e0944617234b935a2a1d88e7
Pedro Oliveira <pluiz@mychirotouch.com>
2022-11-13 20:39:28 -0300

    change name

--- src/factories/use-cases/is-folder-name-availble-factory.ts
+++ src/factories/use-cases/is-folder-name-availble-factory.ts
@@ -1,4 +1,4 @@
-import { IsFolderNameAvailble } from '../../application/use-cases/is-name-availble'
+import { IsFolderNameAvailble } from '../../application/use-cases/is-folder-name-availble'
 import { FindFolderRepository } from '../../data/find-folder-repository'
 
 export function createIsNameAvailble(findFolderRepository: FindFolderRepository) {
