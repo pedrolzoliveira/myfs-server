0d898ee5b8c4d9b6108c5a32c12fe9b4fefc482e
Pedro Oliveira <pluiz@mychirotouch.com>
2022-11-17 21:38:48 -0300

    delete files consumer and publisher

---
+++ src/infra/mqtt/rabbitmq/delete-files-publisher.ts
@@ -0,0 +1,15 @@
+import { Channel } from 'amqplib'
+
+import { DeleteFilesMessage } from '../interfaces/delete-files-queue'
+
+export class DeleteFilesPublisher {
+  constructor(
+    private readonly channel: Channel
+  ) {}
+
+  publish(message: DeleteFilesMessage) {
+    this.channel.sendToQueue('delete-files', Buffer.from(
+      JSON.stringify(message)
+    ))
+  }
+}
